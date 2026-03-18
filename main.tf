929ee2d49b4a9da6602fe997f3ae9b358de2bfc4
clear0111 <clearo2012@yahoo.com>
2026-03-18 13:58:16 +0100

    new version

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
-provider "aws" {
-region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.5.0"
 }
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "terraform-ec2" {
 ami = "ami-02dfbd4ff395f2a1b"
 instance_type = "t3.micro"
