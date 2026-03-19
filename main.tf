351e4b3abc292a7855c2418c50c7d3ec2fdaa117
clear0111 <clearo2012@yahoo.com>
2026-03-19 06:00:05 +0100

    first commit

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "ami-02dfbd4ff395f2a1b"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "GitHubActionsEC2"
