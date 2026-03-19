04430038e17119068e5710270f86f19f33a4f6da
clear0111 <clearo2012@yahoo.com>
2026-03-19 05:39:14 +0100

    version1

--- main.tf
+++ main.tf
@@ -1,20 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 1.7.7"
-    }
-  }
-  required_version = ">= 1.7.7"
-}
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "terraform-ec2" {
-ami = "ami-02dfbd4ff395f2a1b"
-instance_type = "t3.micro"
-tags = {
-Name = "GitHubActionsEC2"
-}
+resource "aws_instance" "example" {
+  ami           = "ami-02dfbd4ff395f2a1b"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "GitHubActionsEC2"
+  }
 }
